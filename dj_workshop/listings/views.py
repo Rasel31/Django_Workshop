07563dd9e53b34bb1d8608d61d431b59b89b673f
rasel Khan <khan.rasel3113@gmail.com>
2020-05-07 23:32:51 +0600

    Added Pagination

--- dj_workshop/listings/views.py
+++ dj_workshop/listings/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Listing
 # from .models import * # Bad Practise
@@ -7,9 +8,23 @@ from .models import Listing
 def listings_index(request):
     listing_list = Listing.objects.all()
 
+    page = request.GET.get('page', 1)
+    paginator = Paginator(listing_list, 2)
+
+    try:
+        listing_list = paginator.page(page)
+    except PageNotAnInteger:
+        # fallback to the first page
+        listing_list = paginator.page(1)
+    except EmptyPage:
+        # probably the user tried to add a page number
+        # in the url, so we fallback to the last page
+        listing_list = paginator.page(paginator.num_pages)
+
     context = {
         'listing_list': listing_list,
     }
+
     return render(request, 'listings/listings.html', context)
 
 
--- dj_workshop/pages/views.py
+++ dj_workshop/pages/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 
 # pages app views
 
+
 def index(request):
     return render(request,'pages/index.html')
+
 
 def about(request):
     return render(request,'pages/about.html')
